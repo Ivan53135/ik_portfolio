b7e6dcc60e89fa60f777e5daff5b26820013cf78
Ivan53135 <ikozlov15.ik@gmail.com>
2020-06-23 18:15:16 +0300

    Small Details

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
-let spinnerWrapper = document.querySelector('.spinner-wrapper');
-
-window.addEventListener('load', function () {
-    // spinnerWrapper.style.display = 'none';
-    spinnerWrapper.parentElement.removeChild(spinnerWrapper);
-});
 $(document).ready(function() {
 
   $(".card").tilt({
