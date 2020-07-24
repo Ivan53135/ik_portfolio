1d97223e5fd23895a831193a5b750c5a4a67afe4
Ivan53135 <ikozlov15.ik@gmail.com>
2020-07-24 16:30:52 +0300

    Still Working

---
+++ assets/works-projects/The Bohrach/app.js
@@ -0,0 +1,5 @@
+var year = new Date().getFullYear();
+
+var date = `Copyright &copy; IK Corporation ${year}. All Rights Reserved.`;
+
+document.getElementsByClassName('copyright_date')[0].innerHTML = date;
--- index.js
+++ index.js
@@ -80,4 +80,10 @@ $(document).ready(function () {
     }
   });
 
+  var year = new Date().getFullYear();
+
+  var date = `Copyright &copy; IK Corporation ${year}. All Rights Reserved.`;
+
+  document.getElementsByClassName('copyright_date')[0].innerHTML = date;
+
 });
